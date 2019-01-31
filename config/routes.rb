b347869167273f315e3f6c7dac5d0b5d49ccde57
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-01-31 15:44:47 +0800

    - Change application.css to application.scss - Add custom.css - Add partial views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'halaman/utama'
+
+  root 'halaman#utama'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
